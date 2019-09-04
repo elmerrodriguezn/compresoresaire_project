6219fce19c33bc8defa920291eccf5da969d8bb7
Elmer Rodriguez <elmer.rodriguezn@gmail.com>
2019-09-04 09:39:49 -0700

    fixed missing self element in query class

--- pages/views.py
+++ pages/views.py
@@ -10,7 +10,16 @@ def index(request):
     
     # Template api function
     query = Query()
-    data = query.get('product.template', 'search_read', [['type', '=', 'product'],['categ_id.parent_id', '=', 71],['x_studio_field_tGMk6', '=', True]], { 'fields': ['name', 'default_code', 'description_sale', 'description', 'create_date'], 'order': 'create_date' })
+    data = query.get(
+        'product.template',
+        'search_read', [
+            ['type', '=', 'product'],
+            ['categ_id.parent_id', '=', 71],
+            ['x_studio_field_tGMk6', '=', True]
+        ],
+        {
+            'fields': ['name', 'default_code', 'description_sale', 'description', 'create_date'], 'order': 'create_date'
+        })
 
     # Django pagination
     paginator = Paginator(data, 20)
@@ -20,20 +29,32 @@ def index(request):
     
     return render(request, 'pages/index.html', context)
 
+
 def contact(request):
     return render(request, 'pages/contact.html')
 
+
 def send_lead(request):
-    fullName = request.POST['fullName']
+    full_name = request.POST['full_name']
     email = request.POST['email']
     phone = request.POST['phone']
     msg = request.POST['msg']
     
     query = Query()
     
-    data = query.create('crm.lead', 'create', {'name': 'compresoresaire.com','contact_name': fullName,'email_from': email,'phone': phone,'description': msg})
+    data = query.create(
+        'crm.lead',
+        'create',
+        {
+            'name': 'compresoresaire.com',
+            'contact_name': full_name,
+            'email_from': email,
+            'phone': phone,
+            'description': msg
+        })
     
     return redirect('/gracias-por-contactarnos/')
-    
+
+
 def thanks(request):
     return render(request, 'pages/thanks.html')
--- products/views.py
+++ products/views.py
@@ -1,26 +1,36 @@
 from django.shortcuts import render, redirect
-from django.core.paginator import Paginator
 from api.query import Query
+
 
 def single(request, id):
     query = Query()
 
-    data = query.get('product.template','search_read',[['type', '=', 'product'],['categ_id.parent_id', '=', 71],['x_studio_field_tGMk6', '=', True],['id', '=', id]],{'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment','create_date'] })
+    data = query.get('product.template', 'search_read',
+                     [['type', '=', 'product'], ['categ_id.parent_id', '=', 71], ['x_studio_field_tGMk6', '=', True],
+                      ['id', '=', id]], {
+                         'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment',
+                                    'create_date']})
 
     context = {"product": data[0]}
-    
+
     return render(request, 'products/single.html', context)
+
 
 def search(request):
     q = request.GET['q']
 
     query = Query()
 
-    data = query.get('product.template','search_read',[['type', '=', 'product'],['categ_id.parent_id', '=', 71],['x_studio_field_tGMk6', '=', True],['name','ilike', q]],{'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment','create_date'], 'limit': 20 })
+    data = query.get('product.template', 'search_read',
+                     [['type', '=', 'product'], ['categ_id.parent_id', '=', 71], ['x_studio_field_tGMk6', '=', True],
+                      ['name', 'ilike', q]], {
+                         'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment',
+                                    'create_date'], 'limit': 20})
 
     context = {"products": data}
-    
+
     return render(request, 'products/search.html', context)
+
 
 def filter(request):
     query = Query()
@@ -31,7 +41,7 @@ def filter(request):
         hp = request.GET['hp']
         if hp:
             value = "{} HP".format(hp)
-    
+
     if 'psig' in request.GET:
         psig = request.GET['psig']
         if psig:
@@ -42,20 +52,28 @@ def filter(request):
         if cfm:
             value = "{} CFM".format(cfm)
 
-    data = query.get('product.template','search_read',[['type', '=', 'product'],['categ_id.parent_id', '=', 71],['x_studio_field_tGMk6', '=', True],['description_sale','like', value]],{'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment','create_date'], 'limit': 20 })
+    data = query.get('product.template', 'search_read',
+                     [['type', '=', 'product'], ['categ_id.parent_id', '=', 71], ['x_studio_field_tGMk6', '=', True],
+                      ['description_sale', 'like', value]], {
+                         'fields': ['id', 'name', 'default_code', 'description_sale', 'description', 'attachment',
+                                    'create_date'], 'limit': 20})
 
     context = {"products": data}
 
     return render(request, 'products/search.html', context)
 
+
 def lead(request):
-    fullName = request.POST['fullName']
+    full_name = request.POST['full_name']
     email = request.POST['email']
     phone = request.POST['phone']
-    description = 'Producto: ' + request.POST['productName'], 'Número de parte: ' + request.POST['pn'], 'Mensaje: ' + request.POST['msg']
-    
+    description = 'Producto: ' + request.POST['productName'], 'Número de parte: ' + request.POST['pn'], 'Mensaje: ' + \
+                  request.POST['msg']
+
     query = Query()
 
-    data = query.create('crm.lead','create',{'name': 'compresoresaire.com','contact_name': fullName,'email_from': email,'phone': phone,'description': description})
+    data = query.create('crm.lead', 'create',
+                        {'name': 'compresoresaire.com', 'contact_name': full_name, 'email_from': email, 'phone': phone,
+                         'description': description})
 
     return redirect('/gracias-por-contactarnos/')
